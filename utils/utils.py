c245eb0160316f101fc49d005068db495365ae5c
bubbliiiiing <3323290568@qq.com>
2023-08-27 21:11:07 +0800

    update seed

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
+import random
+
 import numpy as np
+import torch
 from PIL import Image
 
 
@@ -47,6 +50,27 @@ def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
 
+#---------------------------------------------------#
+#   设置种子
+#---------------------------------------------------#
+def seed_everything(seed=11):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+#---------------------------------------------------#
+#   设置Dataloader的种子
+#---------------------------------------------------#
+def worker_init_fn(worker_id, rank, seed):
+    worker_seed = rank + seed
+    random.seed(worker_seed)
+    np.random.seed(worker_seed)
+    torch.manual_seed(worker_seed)
+
 def preprocess_input(image):
     image /= 255.0
     return image
@@ -62,6 +86,7 @@ def show_config(**kwargs):
         
 def download_weights(phi, model_dir="./model_data"):
     import os
+
     from torch.hub import load_state_dict_from_url
     
     download_urls = {
